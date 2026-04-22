77cd29f681b11ebb2e01bc3927297e9b70dc648c
Manoj <170088914+ManojMsks@users.noreply.github.com>
2026-04-22 23:25:45 +0530

    Added malloc,findfreeblock and allocating space functions

--- malloc.c
+++ malloc.c
@@ -38,3 +38,52 @@ struct block_meta *find_free_block(struct block_meta **last, size_t size) {
   }
   return current;
 }
+
+
+struct block_meta *request_space(struct block_meta* last, size_t size) {
+  struct block_meta *block;
+  block=sbrk(0);
+  void *pointer=sbrk(size+META_SIZE);
+  if(pointer==(void*) -1){
+    return NULL;
+  }
+  assert(block==pointer);
+  if(last){
+    last->next=block;
+  }
+  block->size=size;
+  block->free=0;
+  block->next=NULL;
+  block->magic=0x12345678;
+  return block;
+}
+
+void * malloc(size_t size){
+  struct block_meta *block;
+  if(size<=0){
+    return NULL;
+  }
+  if(!global_base){
+    block=request_space(NULL,size);
+    if(block){
+      global_base=block;
+    }
+    else {
+      return NULL;
+    }
+  }
+  else{
+    struct block_meta *last;
+    last=global_base;
+    block=find_free_block(&last,size);
+    if(block){
+      block->free=0;
+    }else {
+      block=request_space(last,size);
+      if(!block){
+        return NULL;
+      }
+    }
+}
+  return block+1;
+}
