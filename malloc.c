600bf2a59fce0e1662bf1f0799833df21d41ef81
Manoj <170088914+ManojMsks@users.noreply.github.com>
2026-04-25 11:59:06 +0000

    final version but no optimizations

--- malloc.c
+++ malloc.c
@@ -4,16 +4,6 @@
 #include <unistd.h>
 
 
-void *malloc(size_t size) {
-  void *p = sbrk(0);
-  void *request = sbrk(size);
-  if (request == (void*) -1) {
-    return NULL; // sbrk failed.
-  } else {
-    assert(p == request); // Not thread safe.
-    return p;
-  }
-}
 
 
 struct block_meta {
